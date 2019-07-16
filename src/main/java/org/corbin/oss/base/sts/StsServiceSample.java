e820c656a4c653e4ff0d41722f872d4ff01af48c
yinyanbin <yinyanbin@hzquanweitech.com>
2019-07-16 11:07:00 +0800

    添加sts demo

--- src/main/java/org/corbin/oss/base/sts/StsServiceSample.java
+++ src/main/java/org/corbin/oss/base/sts/StsServiceSample.java
@@ -13,7 +13,7 @@ public class StsServiceSample {
 
 //        RoleArn表示的是需要扮演的角色ID，角色的ID可以在角色管理 > 角色详情中找到。
 //        RoleSessionName是一个用来标示临时凭证的名称，一般来说建议使用不同的应用程序用户来区分。
-//        Policy表示的是在扮演角色的时候额外加上的一个权限限制。
+//        Policy表示的是在扮演角色的时候额外加上的一个权限限制,取本地policyyu 角色权限的交集
 //        DurationSeconds指的是临时凭证的有效期，单位是s，最小为900，最大为3600。
 //        id和secret表示的是需要扮演角色的子账号的AccessKey
 
